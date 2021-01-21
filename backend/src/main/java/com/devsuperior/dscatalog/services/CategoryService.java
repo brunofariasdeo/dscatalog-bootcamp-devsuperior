d030d5d8981f6dee630601fc5caea767648a354f
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-20 21:15:24 -0300

    Create DTO for Category classes

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +17,15 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		
+		List<CategoryDTO> listDTO = new ArrayList<>();
+		for (Category cat : list) {
+			listDTO.add(new CategoryDTO(cat));
+		}
+		
+		return listDTO;
 	}
 }
