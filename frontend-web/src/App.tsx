b4c67d1fc5360efd016eaeb1425f8a7f7627c967
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-31 12:35:09 -0300

    Create frontend and navbar component

---
+++ frontend-web/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar';
+
+const App = () => {
+  return(
+    <Navbar />
+  )
+}
+export default App;
---
+++ frontend-web/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const LocalApp = () => {
+  return <h1>Hello World!</h1>
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
