c16420c2c4792afd8d47a3643627344e530a8a5f
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-31 22:06:22 -0300

    Create homepage

--- frontend-web/src/index.tsx
+++ frontend-web/src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const LocalApp = () => {
-  return <h1>Hello World!</h1>
-}
-
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
