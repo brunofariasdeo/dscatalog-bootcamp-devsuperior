ed0e4dff054d0e4c0823c244c074d635a44e5df9
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-02-07 22:15:49 -0300

    Create dynamic product listing

--- frontend-web/src/pages/Catalog/index.tsx
+++ frontend-web/src/pages/Catalog/index.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
+import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
-const Catalog = () => (
-  <div className="catalog-container">
-    <h1 className="catalog-title">
-      Catálogo de produtos
-    </h1>
-    <div className="catalog-products">
-      <Link to="/products/1"><ProductCard /></Link>
-      <Link to="/products/2"><ProductCard /></Link>
-      <Link to="/products/3"><ProductCard /></Link>
-      <Link to="/products/4"><ProductCard /></Link>
-      <Link to="/products/5"><ProductCard /></Link>
-      <Link to="/products/6"><ProductCard /></Link>
-      <Link to="/products/7"><ProductCard /></Link>
-      <Link to="/products/8"><ProductCard /></Link>
-      <Link to="/products/9"><ProductCard /></Link>
+const Catalog = () => {
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+
+  useEffect(() => {
+    const params = {
+      page: 0,
+      linesPerPage: 5
+    }
+
+    makeRequest({ url: '/products', params })
+      .then(response => setProductsResponse(response.data));
+  }, []);
+
+  return (
+    <div className="catalog-container">
+      <h1 className="catalog-title">
+        Catálogo de produtos
+      </h1>
+      <div className="catalog-products">
+        {productsResponse?.content.map(product => (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <ProductCard product={product}/>
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Catalog;
